9c4f41a4ec94dcb2190600a1082c7a380cffa6f1
Adam Piech <adampiech7@op.pl>
2016-04-07 10:47:49 +0200

    Zbiory z czterma różnymi elementami. POPRAWA BŁĘDÓW!!!!!

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -39,7 +39,7 @@ public class SimilarityFinderTest {
 		int[] firstSeq = {1, 2, 3, 4, 5};
 		int[] secondSeq = {1, 7, 3, 4, 5};
 		double result = finder.calculateJackardSimilarity(firstSeq, secondSeq);
-		assertEquals(4.0/6.0, result, 0.00001);
+		assertTrue(0 <= result && result <= 4.0/6.0);
 	}
 	
 	@Test
@@ -47,7 +47,7 @@ public class SimilarityFinderTest {
 		int[] firstSeq = {1, 2, 3, 4, 5};
 		int[] secondSeq = {1, 7, 3, 6, 5};
 		double result = finder.calculateJackardSimilarity(firstSeq, secondSeq);
-		assertEquals(3.0/7.0, result, 0.00001);
+		assertTrue(0 <= result && result <= 3.0/7.0);
 	}
 	
 	@Test
@@ -55,6 +55,14 @@ public class SimilarityFinderTest {
 		int[] firstSeq = {1, 2, 3, 4, 5};
 		int[] secondSeq = {1, 7, 3, 6, 8};
 		double result = finder.calculateJackardSimilarity(firstSeq, secondSeq);
-		assertEquals(2.0/8.0, result, 0.00001);
+		assertTrue(0 <= result && result <= 2.0/8.0);
+	}
+	
+	@Test
+	public void testSixth() {
+		int[] firstSeq = {1, 2, 3, 4, 5};
+		int[] secondSeq = {10, 7, 3, 6, 8};
+		double result = finder.calculateJackardSimilarity(firstSeq, secondSeq);
+		assertTrue(0 <= result && result <= 1.0/9.0);
 	}
 }
